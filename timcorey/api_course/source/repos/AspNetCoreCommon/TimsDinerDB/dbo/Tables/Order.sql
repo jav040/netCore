edaae8ba1bb6968d83895318987a9c5728ad54aa
Your Name <you@example.com>
2024-07-07 17:32:46 -0700

    removed submodules creating errors

--- timcorey/api_course/source/repos/AspNetCoreCommon/TimsDinerDB/dbo/Tables/Order.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Order]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [OrderName] NVARCHAR(50) NOT NULL, 
-    [OrderDate] DATETIME2 NOT NULL, 
-    [FoodId] INT NOT NULL, 
-    [Quantity] INT NOT NULL, 
-    [Total] MONEY NOT NULL
-)
